077c706071b5740d54b0816feebf4a5c1023612e
Ведерников Сергей <swnike@mail.ru>
2024-09-22 10:03:59 +0500

    research format code

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,9 +8,9 @@
 
             Console.WriteLine(MyName);
 
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine("Мне 27 лет");
-            Console.WriteLine("My name is Jane");
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\tМне 27 лет");
+            Console.WriteLine("\tMy name is Jane");
 
             Console.ReadKey();
         }
