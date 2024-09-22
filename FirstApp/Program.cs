4b2d29963e8f790369d13ca81e3f5f4940c8e9c5
Ведерников Сергей <swnike@mail.ru>
2024-09-22 10:01:51 +0500

    research Literals

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,7 +5,13 @@
         static void Main(string[] args)
         {
             const string MyName = "Jane";
+
             Console.WriteLine(MyName);
+
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine("Мне 27 лет");
+            Console.WriteLine("My name is Jane");
+
             Console.ReadKey();
         }
     }
