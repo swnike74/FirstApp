f5856b0fc790b6ab2c7a858231c119ad5874232a
Ведерников Сергей <swnike@mail.ru>
2024-09-22 10:11:35 +0500

    symbol literals

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,10 +8,11 @@
 
             Console.WriteLine(MyName);
 
-            Console.WriteLine("\tПривет, мир");
-            Console.WriteLine("\tМне 27 лет");
-            Console.WriteLine("\tMy name is Jane");
-
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
             Console.ReadKey();
         }
     }
