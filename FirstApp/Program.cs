ecf81772ac704ee641a38413135a66bd0b2d6214
Ведерников Сергей <swnike@mail.ru>
2024-09-25 21:22:37 +0500

    data type conversion

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,14 +8,40 @@
             byte MyAge = 27;
             bool HaveIApet = true;
             double MyShoeSize = 37.5;
+            DaysOfWeek MyFavoriteDay;
 
             Console.WriteLine("My name is " + MyName);
             Console.WriteLine("MyAge " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIApet);
             Console.WriteLine("My shoe size is " + MyShoeSize);
 
+            Console.WriteLine("IntMin {0} ", int.MinValue);
+            Console.WriteLine("IntMax {0} ", int.MaxValue);
+
+            MyFavoriteDay = DaysOfWeek.Friday;
+
+            Console.WriteLine(MyFavoriteDay);
+            Console.WriteLine("\tПривет,\nмир!");
+
+            Console.WriteLine($"Мой возраст\t{MyAge}");
+
+            
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+  
 
             Console.ReadKey();
+        }
+        enum DaysOfWeek : byte
+        {
+            Tuesday,
+            Monday,
+            Wednesday,
+            Friday
         }
     }
 }
